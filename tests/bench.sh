e21916adc1450f9e98310a8220559174b2729d9c
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-06-27 16:22:43 +0200

    with type_enclosing

--- tests/bench.sh
+++ tests/bench.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+function prbench {
+     jq '{"results": [{"name": "'$1'", "metrics": [{"name": "'$2':'$3' '$4'", "value": .timing.query, "units": "ms"}]}]}'
+}
 function locate {
-     sed -n "$1"p "$3"
-     ocamlmerlin single locate -look-for ml -position $1:$2 -filename $3 < $3 \
-     | jq '{"results": [{"name": "locate", "metrics": [{"name": "'$1':'$2' '$3'", "value": .timing.query, "units": "ms"}]}]}'
+     sed -n "$2"p "$4"
+     printf "%$3s^\n" ' '
+     ocamlmerlin single $1 -look-for ml -position $2:$3 -filename $4 < $4 \
+     | prbench $1 $2 $3 $4
 }
-function type_enclosing {
-     sed -n "$1"p "$3"
-     ocamlmerlin single type-enclosing -position $1:$2 -verbosity 0 < $3 \
-     | jq '{"results": [{"name": "type_enclosing", "metrics": [{"name": "'$1':'$2' '$3'", "value": .timing.query, "units": "ms"}]}]}'
+function type-enclosing {
+     sed -n "$2"p "$4"
+     printf "%$3s^\n" ' '
+     ocamlmerlin single $1 -position $2:$3 -verbosity 0 < $4 \
+     |prbench $1 $2 $3 $4
 }
 
-locate 23 16 ./examples/irmin_git_store.ml
-locate 118 21 ./examples/process.ml
-locate 49 20 ./examples/push.ml
-locate 34 16 ./examples/deploy.ml
-locate 111 9 ./examples/process.ml
-locate 43 26 ./examples/push.ml
-locate 70 22 ./test/irmin-graphql/common.ml
+locate locate 23 16 ./examples/irmin_git_store.ml 
+locate locate 118 21 ./examples/process.ml 
+locate locate 49 20 ./examples/push.ml 
+locate locate 34 16 ./examples/deploy.ml 
+locate locate 111 9 ./examples/process.ml 
+locate locate 43 26 ./examples/push.ml 
+locate locate 70 22 ./test/irmin-graphql/common.ml 
 
-type_enclosing 95 9 ./src/irmin/watch.ml
-type_enclosing 96 3 ./src/irmin/watch.ml
-type_enclosing 215 13 ./src/irmin/watch.ml
-type_enclosing 86 21 ./src/irmin/watch.ml
-type_enclosing 95 22 ./src/irmin/watch.ml
-type_enclosing 207 38 ./src/irmin/watch.ml
-type_enclosing 236 36 ./src/irmin/watch.ml
-type_enclosing 240 43 ./src/irmin/watch.ml
-type_enclosing 303 26 ./src/irmin/watch.ml
-type_enclosing 317 17 ./src/irmin/watch.ml
+type-enclosing type-enclosing 215 13 ./src/irmin/watch.ml 
+type-enclosing type-enclosing 86 21 ./src/irmin/watch.ml 
+type-enclosing type-enclosing 95 22 ./src/irmin/watch.ml 
+type-enclosing type-enclosing 240 43 ./src/irmin/watch.ml 
+type-enclosing type-enclosing 303 26 ./src/irmin/watch.ml 
+type-enclosing type-enclosing 317 17 ./src/irmin/watch.ml 
